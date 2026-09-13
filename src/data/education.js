68733b3311b2c7004898c9e340e1b3d7a8e78879
sow-aliou <sow8.aliou@gmail.com>
2026-09-13 10:58:41 +0000

    fix: corrige les annees de la Licence (2022 - 2026)

--- src/data/education.js
+++ src/data/education.js
@@ -1,7 +1,7 @@
 export const education = [
   {
     degree: 'Licence en Informatique (Génie Logiciel)',
-    school: 'Université Iba Der Thiam de Thiès (UIDT) | 2025 - 2026',
+    school: 'Université Iba Der Thiam de Thiès (UIDT) | 2022 - 2026',
   },
   {
     degree: 'Baccalauréat Série STIDD (Sciences et Technologies de l’Industrie et du Développement Durable)',
